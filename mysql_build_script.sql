9d0d67b83007133bf4c71ac0dbba475e6dd1e8d9
Kirby Olson <riknos314@gmail.com>
2015-09-29 14:58:04 -0500

    update order of sql creation script

--- mysql_build_script.sql
+++ mysql_build_script.sql
@@ -2,8 +2,14 @@ CREATE USER senior;
 GRANT ALL PRIVILEGES ON *.* TO senior;
 CREATE DATABASE lost;
 USE lost;
+CREATE TABLE LocationLF (locationID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+phonenumber VARCHAR(20) NOT NULL,
+email VARCHAR(50) NOT NULL,
+building_name VARCHAR(20) NOT NULL, 
+room_name VARCHAR(30) NOT NULL)
+engine=InnoDB;
 
-# Admin Table
+
 
 CREATE TABLE AdminLF (norsekeyID varchar(30) NOT NULL PRIMARY KEY, 
 password VARCHAR(128) NOT NULL,
@@ -14,16 +20,7 @@ email VARCHAR(50) NOT NULL,
 FOREIGN KEY (locationID) REFERENCES LocationLF(locationID))
 engine=InnoDB;
 
-# Location Table
 
-CREATE TABLE LocationLF (locationID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-phonenumber VARCHAR(20) NOT NULL,
-email VARCHAR(50) NOT NULL,
-building_name VARCHAR(20) NOT NULL, 
-room_name VARCHAR(30) NOT NULL)
-engine=InnoDB;
-
-# Item Table
 
 CREATE TABLE ItemLF (itemID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 time_stamp DATE NOT NULL,
@@ -37,7 +34,6 @@ FOREIGN KEY (locationID) REFERENCES LocationLF(locationID),
 FOREIGN KEY (accepted_by) REFERENCES AdminLF(norsekeyID))
 engine=InnoDB;
 
-# Image Table
 
 CREATE TABLE ImageLF (imageID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 image_type VARCHAR(25) NOT NULL DEFAULT '',
