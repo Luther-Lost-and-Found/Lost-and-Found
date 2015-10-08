d92ad58f50166438d72f70b87a1e4f03ad15771c
alevar <varaal01@luther.edu>
2015-10-08 14:03:50 -0500

    Configuration Scripts for establishing modular communication with mysql

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'port' : '3306',
+        'user': 'senior',
+        'password': 'qwerty'
+    },
+	'database': 'lost',
+    'users_table': 'AdminLF'
+};
