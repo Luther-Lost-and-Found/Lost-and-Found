3de09b0ddb755c106f049238375e74f81067a6bc
alevar <varaal01@luther.edu>
2015-10-15 13:35:53 -0500

    Angular Controller with minimal post functionality

--- public/controllers/logincontroller.js
+++ public/controllers/logincontroller.js
@@ -1,11 +1,14 @@
-var myApp = angular.module('LostApp', []);
-myApp.controller('loginController', ['$timeout', '$scope', '$http', function($timeout, $scope, $http) {
-  console.log("Hello World from logincontroller");
+var loginApp = angular.module('LoginApp', []);
+loginApp.controller('loginController', ['$timeout', '$scope', '$http', function($timeout, $scope, $http) {
+	console.log("Hello World from controller");
 
-  $scope.loginSubmit = function($scope){
-    console.log($scope.username);
-    $http.post("/login",$scope.username).success(function(response){
-      refresh();
-    });
 
-  }
+	$scope.loginSubmit = function(){
+		console.log($scope.username);
+		$scope.username = "hello";
+		$http.post("/login",$scope.username).success(function(response){
+			console.log("success");
+		});
+	}
+}]);
+
