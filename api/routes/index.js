60f0123c58e50ec4f40f205533d7985976802e2b
alevar <varaal01@luther.edu>
2015-10-17 23:55:16 -0500

    Added rendering of the front-end. #7

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express'),
-    path = require('path');
+    path = require('path'),
+    express = require('express');
+
 
 module.exports = function(app, db, isLoggedIn) {
+	console.log("please work");
+	app.use(express.static(path.join(__dirname, '/public/login')));
+	app.engine('.html', require('ejs').__express);
     app.get('/', function(req, res) {
-        res.sendFile(path.join(app.locals.rootDir + '/public/login/login.html'));
+        res.render(path.join(app.locals.rootDir + '/public/login/login.html'));
     });
 };
