4f10e66263c6f6015ca7fbb93ef42505ce7cbede
alevar <varaal01@luther.edu>
2015-10-17 21:18:34 -0500

    Modified passport configuration in accordance with our database. #7

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,12 @@
 // config/passport.js
 
 // load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
-
-// load up the user model
-var mysql = require('mysql');
-var dbconfig = require('./database');
-var connection = mysql.createConnection(dbconfig.connection);
+var LocalStrategy = require('passport-local').Strategy,
+    bcrypt = require('bcrypt-nodejs'),
+    mysql = require('mysql'),
+    dbconfig = require('./database'),
+    connection = mysql.createConnection(dbconfig.connection);
+    
 
 
 console.log("passport");
@@ -22,12 +22,12 @@ module.exports = function(passport) {
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+        done(null, user);
     });
 
     // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
+    passport.deserializeUser(function(user, done) {
+        connection.query("SELECT * FROM AdminLF WHERE norsekeyID = ? ",[id], function(err, rows){
             done(err, rows[0]);
         });
     });
@@ -81,26 +81,36 @@ module.exports = function(passport) {
     // by default, if there was no name, it would just be called 'local'
 
     passport.use(
-        'local-login',
+        'local',
         new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
             usernameField : 'username',
             passwordField : 'password',
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) { // callback with email and password from our form
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
-                if (err)
+        function(req, usernameField, passwordField, done) { // callback with email and password from our form
+            connection.query("SELECT * FROM AdminLF WHERE norsekeyID = ?",[usernameField], function(err, rows){
+                console.log(usernameField);
+                console.log(rows[0].password);
+                if (err){
+                    console.log("hello1");
                     return done(err);
+                }
+                console.log("hey sexy");
                 if (!rows.length) {
+                    console.log("hello2");
                     return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
                 }
-
+                console.log("hey sexy");
                 // if the user is found but the password is wrong
-                if (!bcrypt.compareSync(password, rows[0].password))
+                if (passwordField != rows[0].password){
+                    console.log("hello3");
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                }
 
                 // all is well, return successful user
+
+                console.log("hey sexy");
                 return done(null, rows[0]);
             });
         })
