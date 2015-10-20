e096265bbf2371672599a9ff0f297452836efc13
alevar <varaal01@luther.edu>
2015-10-20 00:44:08 -0500

    Modified the node index to load the necessary configurations for SPA. #8

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,9 @@ var express = require('express'),
 
 
 module.exports = function(app, db, isLoggedIn) {
-	console.log("please work");
 	app.use(express.static(path.join(__dirname, '/public/login')));
 	app.engine('.html', require('ejs').__express);
     app.get('/', function(req, res) {
-        res.render(path.join(app.locals.rootDir + '/public/login/login.html'));
+        res.render(path.join(app.locals.rootDir + '/public/index.html'));
     });
 };
