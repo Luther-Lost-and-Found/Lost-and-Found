3dfff92ca1ed6886eb8f84c8e3d6bdffa2a47df9
alevar <varaal01@luther.edu>
2015-10-27 22:01:16 -0500

    Updated Script that uses hashed password and populates the database with lost items.

--- build.sql
+++ build.sql
@@ -10,8 +10,6 @@ building_name VARCHAR(20) NOT NULL,
 room_name VARCHAR(30) NOT NULL)
 engine=InnoDB;
 
-
-
 CREATE TABLE AdminLF (norsekeyID varchar(30) NOT NULL PRIMARY KEY, 
 password VARCHAR(128) NOT NULL,
 locationID INT NOT NULL,
@@ -43,5 +41,58 @@ FOREIGN KEY (locationID) REFERENCES LocationLF(locationID),
 FOREIGN KEY (accepted_by) REFERENCES AdminLF(norsekeyID))
 engine=InnoDB;
 
+USE lost
 
+INSERT INTO LocationLF (locationID,phonenumber,email,building_name,room_name)
+VALUES (1,"111-1111","awesome@location.edu","Miller","418");
 
+INSERT INTO AdminLF (norsekeyID,password,locationID,first_name,last_name,email)
+VALUES ("111111", "$2a$10$p1LerZ.GmYkKRHNQS5UPyeZAjhEJ9uFgn2kEmns0mmTsqr/b/5bRi", 1, "Super", "Senior", "super@senior.edu");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (1,CURDATE(),"Bag","Blue",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (2,CURDATE(),"Bottle","Third in a row",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (3,CURDATE(),"Wallet","Empty",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (4,CURDATE(),"Pajama","Cozy",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (5,CURDATE(),"Palmer","Happy",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (6,CURDATE(),"Kylie","Black",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (7,CURDATE(),"Wires","Red",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (8,CURDATE(),"Doggie","Rainbow Colors",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (9,CURDATE(),"Consciousness","Undefined",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (10,CURDATE(),"Textbook","With a lot of pictures",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (11,CURDATE(),"Launch","Rotten",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (12,CURDATE(),"Coat","Black",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (13,CURDATE(),"Marker","Family Relic",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (14,CURDATE(),"Charger","MacBook Pro",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (15,CURDATE(),"Phone","Keep it",1,"111111","False");
+
+INSERT INTO ItemLF (itemID,time_stamp,title,tags,locationID,accepted_by,claimed)
+VALUES (16,CURDATE(),"Game","Very sad",1,"111111","False");
