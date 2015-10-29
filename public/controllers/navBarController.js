8948f9ae333992e2119cd8eddff523b4633f7a73
Jessica Tan <jatan94@gmail.com>
2015-10-29 17:14:03 -0500

    adding a popup for add item

--- public/controllers/navBarController.js
+++ public/controllers/navBarController.js
@@ -1,5 +1,5 @@
-angular.module('navBarApp').controller('NavBarCtrl', ['$timeout', '$scope', '$http', '$window',
-	function($timeout, $scope, $http, $window) {
+angular.module('navBarApp',['ui.bootstrap']).controller('NavBarCtrl', ['$timeout', '$scope', '$http', '$window','$animate','$uibModal','$rootScope',
+	function($timeout, $scope, $window, $http, $animate,$uibModal,$rootScope ) {
 
 	console.log("Hello World from the Navigation Bar");
 
@@ -14,4 +14,39 @@ angular.module('navBarApp').controller('NavBarCtrl', ['$timeout', '$scope', '$ht
 		$window.location.href = "/#searchItem"
 	}
 
+	$scope.addItem = function(){
+		  var modalInstance = $uibModal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: 'myNavContent.html',
+	      controller: 'ModalInstanceCtrl',
+	      size: 'lg',
+	      resolve: {
+	        items: function () {
+	          	return $scope.item;
+	        }
+	      }
+	    });
+	};	
 }]);
+
+//create a service between navbar controller and itemlistcontroller trigger an event,
+//$scope.editItem = function ($element) {
+		
+
+angular.module('navBarApp').controller('ModalInstanceCtrl', function ($http,$rootScope,$scope, $uibModalInstance, items) {
+
+	$scope.addItem = function(){
+
+		console.log($scope.item);
+		$http.post("/additem",$scope.item).success(function(response){
+			console.log("yes");
+
+		});
+
+	}
+
+ 	$scope.cancel = function () {
+    	$uibModalInstance.dismiss('cancel');
+  	};
+});
+       
