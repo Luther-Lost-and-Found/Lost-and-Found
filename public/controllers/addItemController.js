802e460213275a2ddd992c2becbe56c86568cba9
alevar <varaal01@luther.edu>
2015-11-01 14:19:19 -0600

    Modified controllers to accomodate the image analysis handling

---
+++ public/controllers/addItemController.js
@@ -0,0 +1,79 @@
+var app = angular.module('addItemApp', ['ui.bootstrap','ngFileUpload']);
+
+app.controller('addItemCtrl', ['$timeout', '$scope', '$http', '$window',
+	'$animate','$uibModal','$rootScope','sharedServiceUploadModal',
+	function($timeout, $scope, $http, $window, $animate, $uibModal,$rootScope, sharedServiceUploadModal) {
+
+	console.log("Hello World from the addItem Controller");
+
+	$scope.addItem = function () {
+
+		sharedServiceUploadModal.setProperty($scope.item);
+
+	    var modalInstance = $uibModal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: 'partials/addItem/upload.html',
+	      controller: 'uploadController',
+	      size: 'lg',
+	      resolve: {
+	        items: function () {
+	          	return $scope.item;
+	        }
+	      }
+	    });
+
+	    modalInstance.result.then(function ($rootScope) {
+	      $rootScope.selected = selectedItem;
+	    });
+
+	    $scope.$on('handleBroadcast', function() {
+        	refresh();
+    	});
+	};
+}]).$inject = ['$scope', 'sharedServiceUploadModal'];
+
+app.controller('uploadController',
+	function ($http,$rootScope,$scope, $uibModalInstance, items, Upload,sharedServiceUploadModal) {
+ 	$scope.uploadImage = function (file) {
+ 		if (file != null) {
+	    	$scope.upload(file)
+	    }
+    	$uibModalInstance.dismiss('cancel');
+  	};
+
+  	$scope.upload = function(image) {
+	    $scope.errorMsg = null;
+	    var transferredItem = sharedServiceUploadModal.getProperty();
+
+	    $http.post("/additem",transferredItem).success(function(response){
+			console.log("added successfully");
+
+			var fileExtension = '.' + image.name.split('.').pop();
+
+			console.log(response.toString());
+
+		    Upload.rename(image, response.toString()+fileExtension);
+
+		    console.log(image);
+
+		    image.upload = Upload.upload({
+		      	url: 'additem/uploadImage',
+		      	method: 'POST',
+		      	data: {
+			    	type: 'profile'
+			    },
+			    file: image
+		    });
+		});
+	};
+
+	$scope.getReqParams = function () {
+	    return $scope.generateErrorOnServer ? '?errorCode=' + $scope.serverErrorCode +
+	    '&errorMessage=' + $scope.serverErrorMsg : '';
+	};
+
+  	$scope.cancel = function () {
+    	$uibModalInstance.dismiss('cancel');
+  	};
+  	
+}).$inject = ['$scope', 'sharedServiceUploadModal'];
---
+++ public/controllers/main_controller.js
@@ -0,0 +1,109 @@
+'use strict';
+
+angular.module('LoginApp',[]);
+angular.module('ItemApp',[]);
+angular.module('navBarApp',[]);
+angular.module('addItemApp',[]);
+angular.module('searchItemApp',[]);
+
+var myApp = angular.module('LostApp', ['ui.router','LoginApp','ItemApp','navBarApp','addItemApp','searchItemApp']);
+
+myApp.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+    function($stateProvider,$urlRouterProvider, $httpProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+        	.state('login',{
+                url: '/',
+                views: {
+                    'login': {
+                        templateUrl : 'partials/login/login.html',
+                        action : 'LoginApp.loginController'
+                    }
+                }
+        	})
+            .state('itemlist', {
+                url:'/itemlist',
+                views: {
+                    'navBar': {
+                        templateUrl : '../partials/navBar/navBar.html',
+                        action : 'navBarApp.NavBarCtrl'
+                    },
+                    'itemlist': {
+                        templateUrl : '../partials/itemList/itemList.html',
+                        action : 'ItemApp.ItemCtrl'
+                    }
+                }
+            })
+            .state('additem', {
+                url:'/additem',
+                views: {
+                    'navBar': {
+                        templateUrl : '../partials/navBar/navBar.html',
+                        action : 'navBarApp.NavBarCtrl'
+                    },
+                    'addItem': {
+                        templateUrl : '../partials/addItem/addItem.html',
+                        action : 'addItemApp.addItemCtrl'
+                    }
+                }
+            })
+            .state('searchitem', {
+                url:'/searchItem',
+                views: {
+                    'navBar': {
+                        templateUrl : '../partials/navBar/navBar.html',
+                        action : 'navBarApp.NavBarCtrl'
+                    },
+                    'searchitem': {
+                        templateUrl : '../partials/search/searchItem.html',
+                        action : 'searchItemApp.searchItemController'
+                    }
+                }
+            })
+
+        var interceptor = ['$location', '$q', '$injector', function($location, $q, $injector) {
+            console.log("inside the interceptor"); 
+            return {
+                response: function(response) {
+                    // do something on success 
+                    return response; 
+                },
+                responseError: function(response) { 
+                    if (response.status === 401) 
+                        $location.url('/login'); 
+                    return $q.reject(response); 
+                } 
+            };
+        }];
+        
+        $httpProvider.interceptors.push(interceptor);
+    }
+]);
+
+myApp.service('sharedProperties',function(){
+    var property = {nice:'First'};
+
+    return {
+        getProperty: function () {
+            return property;
+        },
+        setProperty: function(value) {
+            property = value;
+        }
+    };
+});
+
+myApp.service('sharedServiceUploadModal',function(){
+    var property = {nice:'First'};
+
+    return {
+        getProperty: function () {
+            return property;
+        },
+        setProperty: function(value) {
+            property = value;
+        }
+    };
+});
