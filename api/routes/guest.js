198b47832481f0712fe5fb6552980d6f0dadc43d
harrmi02 <harrmi02@luther.edu>
2015-11-12 15:37:39 -0600

    Guest search modal appearance working, no functionality yet

--- api/routes/guest.js
+++ api/routes/guest.js
@@ -9,9 +9,9 @@ db.query('USE ' + dbconfig.database);
 module.exports = function(app) {
     app.get("/guestPage", function(req,res){
             console.log('i received the request');
-
-            db.query('SELECT * from ItemLF', function(err, rows, fields) {
-                    
+            //var to_search = Object.keys(req.query)[0];
+            //db.query("SELECT ItemLF.title, LocationLF.building_name from ItemLF, LocationLF WHERE Concat(ItemLF.title,'',ItemLF.tags) like '%" + to_search + "%'", function(err, rows, fields) {
+            db.query("SELECT * from LocationLF", function(err, rows, fields){        
                     console.log(rows);
                     //console.log(auth.user.username);
                     res.json(rows);
--- public/controllers/guestController.js
+++ public/controllers/guestController.js
@@ -1,11 +1,11 @@
 angular.module('guestApp',['ui.bootstrap']).controller('guestController', ['$timeout', '$scope', '$http',
-	'$animate','$uibModal','$rootScope',
-	function($timeout, $scope, $http, $animate,$uibModal,$rootScope) {
+	'$animate','sharedProperties','$uibModal','$rootScope',
+	function($timeout, $scope, $http, $animate, sharedProperties, $uibModal, $rootScope) {
 
 	console.log("Hello World from controller");
 
 	var refresh = function(){
-		$http.get("/itemlist").success(function(response){
+		$http.get("/guest").success(function(response){
 			console.log('i got the data requested');
 			console.log(response);
 			$scope.$applyAsync(function(){
@@ -16,7 +16,49 @@ angular.module('guestApp',['ui.bootstrap']).controller('guestController', ['$tim
 	};
 
 	$scope.submitGuestSearch = function($scope){
-		console.log($scope);
-
+		$rootScope.item = {};
+		var modalInstance = $uibModal.open({
+	    	animation: $scope.animationsEnabled,
+	    	templateUrl: 'guestSearchResult.html',
+	    	controller: 'guestModalInstanceCtrl',
+	    	size: 'lg',
+	    	resolve: {
+	        	items: function () {
+	          		return $scope.item;
+	        	}
+	    	}
+	    });
 	};
 }]);
+
+angular.module('guestApp').controller('guestModalInstanceCtrl', function ($http,$rootScope,$scope, $uibModalInstance, items,sharedService) {
+
+	$scope.submitGuestSearch = function(){
+
+		var current_search = sharedProperties.getProperty().title;
+
+		$http.get("/guestPage/?"+current_search).success(function(response){
+			console.log("should be good");
+			console.log(response);
+			$scope.$applyAsync(function(){
+				$scope.locationlist = response;
+			});
+		});
+
+		console.log($scope.locationlist);
+		$http.post("/guestPage",$scope.locationlist).success(function(response){
+			console.log("yes");
+
+			
+			sharedService.refreshMain();
+
+			$uibModalInstance.dismiss('cancel');
+
+		});
+
+	}
+
+ 	$scope.cancel = function () {
+    	$uibModalInstance.dismiss('cancel');
+  	};
+});
