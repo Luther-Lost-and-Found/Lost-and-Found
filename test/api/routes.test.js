6c6a14b15d556db0b42c5f31e178852795f80725
Kirby Olson <riknos314@gmail.com>
2015-12-03 13:19:55 -0600

    add testing for auth 404 and non-auth 401 functionalities

--- test/api/routes.test.js
+++ test/api/routes.test.js
@@ -1,8 +1,12 @@
 var chai = require('chai'),
     chaiHttp = require('chai-http'),
+    http = require('http'),
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai'),
     expect = chai.expect;
 
 chai.use(chaiHttp);
+chai.use(sinonChai);
 
 describe('requests on undefined routes', function () {
 
@@ -10,30 +14,67 @@ describe('requests on undefined routes', function () {
 
     beforeEach(function() {
         delete require.cache[require.resolve('../../server')];
-        server = require('../../server.js');
+        server = require('../../server.js').server;
     });
 
     afterEach(function(done) {
         server.close(done);
     });
 
-    it('should have status 401', function(done) {
-        chai.request(server)
-            .get('/asdfjkasjdkfajslfdjalks/')
-            .end(function(err, res){
-                expect(err).to.not.be.ok;
-                expect(res).to.have.status(401);
-                done();
-            });
+    describe('unauthenticated requests', function () {
+
+        it('should have status 401', function(done) {
+            chai.request(server)
+                .get('/asdfjkasjdkfajslfdjalks/')
+                .end(function(err, res){
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.status(401);
+                    done();
+                });
+        });
+        it('should redirect to /', function(done) {
+            chai.request(server)
+                .get('/asdfjkasjdkfajslfdjalks/')
+                .end(function(err, res){
+                    expect(err).to.not.be.ok;
+                    expect(res.text).to.contain('Redirecting to /');
+                    done();
+                });
+        });
     });
-    it('should redirect to /', function(done) {
-        chai.request(server)
-            .get('/asdfjkasjdkfajslfdjalks/')
-            .end(function(err, res){
-                expect(err).to.not.be.ok;
-                expect(res.text).to.contain('Redirecting to /');
-                done();
-            });
+    describe('authenticated requests', function () {
+
+        beforeEach(function() {
+            // overriding authentication checking
+            // will treat every request in this describe block as authenticated
+            req = http.IncomingMessage.prototype;
+            stub = sinon.stub(req, 'isAuthenticated');
+            stub.returns(true);
+            // mocked user
+            req.user = {
+                norsekeyID: '111111',
+                password: '$2a$10$p1LerZ.GmYkKRHNQS5UPyeZAjhEJ9uFgn2kEmns0mmTsqr/b/5bRi',
+                locationID: 1,
+                first_name: 'Super',
+                last_name: 'Senior',
+                email: 'super@senior.edu' };
+        });
+
+        afterEach(function() {
+            // restore auth to normal
+            stub.restore();
+            delete req.user;
+        });
+
+        it('should have status 404', function(done) {
+            chai.request(server)
+                .get('/asdfjkasjdkfajslfdjalks/')
+                .end(function(err, res){
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.status(404);
+                    done();
+                });
+        });
     });
 
 })
