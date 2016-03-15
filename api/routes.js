5cfbaf2ac8c7b82e28399deb4be2a815a10eec5c
sparrow <alevar@gmail.com>
2016-03-15 15:17:33 -0500

    Improved authentication handling

--- api/routes.js
+++ api/routes.js
@@ -4,23 +4,24 @@ var express = require('express'),
 module.exports = function(app, passport) {
     require('./routes/login.js')(app, passport)
     require('./routes/itemlist.js')(app, passport, isLoggedIn)
-    require('./routes/logout.js')(app, passport, isLoggedIn)
+    require('./routes/logout.js')(app, passport)
     require('./routes/additem.js')(app, passport, isLoggedIn)
     require('./routes/search.js')(app, passport, isLoggedIn)
     require('./routes/tagsRoute.js')(app, passport, isLoggedIn)
     require('./routes/guest.js')(app)
 
-    app.all('*', isLoggedIn, function(req, res) {
-        console.log("This shouldn't be called");
-        res.redirect('/');
-        res.status(401);
+    app.get('*',isLoggedIn, function(req, res){
+      console.log("404 happened in routes");
+      res.status(404).end();
     });
 
     app.all('/loggedin', function(req, res) {
+        console.log("does this even happen???");
         res.send(req.isAuthenticated() ? req.user : '0');
     });
-};
 
+    
+};
 
 function isLoggedIn(req, res, next) {
 
