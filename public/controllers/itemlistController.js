1d290c89beefa70155a078b9a0059552ac43c6fd
sparrow <alevar@gmail.com>
2016-03-15 15:23:33 -0500

    Replacement of bootstrap with Material. Disintegration of controllers(additem) into separate instance

--- public/controllers/itemlistController.js
+++ public/controllers/itemlistController.js
@@ -1,203 +1,411 @@
-angular.module('ItemApp',['ui.bootstrap','ngMaterial']).controller('ItemCtrl', ['$timeout', '$scope', '$http',
-	'$animate','$uibModal','$rootScope','sharedService',
-	function($timeout, $scope, $http, $animate,$uibModal,$rootScope,sharedService) {
+var app = angular.module('ItemApp',['ui.bootstrap','ngMaterial']);
 
-	console.log("Hello World from controller");
+app.controller('ItemCtrl', function($timeout, $scope, $http, $animate,$rootScope,sharedService,sharedServiceUploadModal,$mdDialog, $mdMedia) {
 
-	var refresh = function(){
-		$http.get("/itemlist").success(function(response){
-			console.log('i got the data requested');
-			console.log(response);
-			$scope.$applyAsync(function(){
-				$scope.itemlist = response;
-				$scope.item = "";
-			});
-		});
-	};
+  var refresh = function(){
+    $http.get("/itemlist").success(function(response){
+      $scope.$applyAsync(function(){
+        console.log(response);
+        $scope.itemlist = response;
+        $scope.item = "";
+      });
+    });
+  };
 
-	refresh();
+  refresh();
 
-	$scope.sort = function(sortMethod){
-	};
+  $scope.addItem = function(ev){
+    sharedServiceUploadModal.setProperty($scope.item);
+    $rootScope.item = {};
 
-	$scope.updateItem = function($element){
-		var current_id = ($element.itemID);
-		$http.put("/itemlist/" + current_id, $scope.item).success(function(response){
-			console.log("UPDATE");
-			refresh();
-		});
-	};
+    $mdDialog.show({
+      controller: itemModalInstanceCtrl,
+      templateUrl: 'partials/itemList/addItem.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      scope: $scope.$new()
+    })
+    // var modalInstance = $uibModal.open({
+    //     animation: $scope.animationsEnabled,
+    //     templateUrl: 'partials/itemList/addItem.html',
+    //     controller: 'itemModalInstanceCtrl',
+    //     size: 'lg',
+    //     resolve: {
+    //         items: function () {
+    //             return $scope.item;
+    //         }
+    //     }
+    //   });
+  };
 
-	$scope.clicked = function($element){
-		var current_id = ($element.itemID); 
-		console.log($element);
-		$http.get("/itemlist/" + current_id).success(function(response){
-			console.log("got the data to edit");
-			var itemTags = [];
-			for(i=0;i<response.length;i++){
-				itemTags.push(response[i].tag);
-			}
-			console.log("HERE IS THE ITEMTAGSLIST");
-			$rootScope.itemTags = itemTags;
-			console.log($rootScope.itemTags);
-			$rootScope.$applyAsync(function(){
-				$rootScope.item = response[0];
-			});
-		});
+  $scope.deleteAll = function(){
+    var foundToDelete = 0;
+    for (i=0;i<$scope.itemlist.length;i++){
+      if ($scope.itemlist[i].toDelete == true){
+        foundToDelete++;
+        var current_id = ($scope.itemlist[i].itemID); 
+        $http.delete("/itemlist/?" + current_id).success(function(response){
+        });
+      } 
+    }
+    if(foundToDelete > 0){
+      refresh();
+    }
+  };
 
-	    var modalInstance = $uibModal.open({
-		    animation: $scope.animationsEnabled,
-		    templateUrl: 'myModalContent.html',
-		    controller: 'ModalInstanceCtrl',
-		    size: 'md',
-		    resolve: {
-		        items: function () {
-		          	return $scope.item;
-		        }
-	      	}
-	    });
-    };
+  $scope.sortAlpha = function(){
+    $http.get("/itemlist/alpha").success(function(response){
+      $scope.$applyAsync(function(){
+        $scope.itemlist = response;
+        $scope.item = "";
+      });
+    });
+  };
 
-	$scope.showDetails = function ($element) {
-	    if ($scope.active != $element.title) {
-	    	$scope.active = $element.title;
-	    }
-	    else {
-	    	$scope.active = null;
-	    }
-	};
+  $scope.sortLoc = function(){
+    $http.get("/itemlist/location").success(function(response){
+      $scope.$applyAsync(function(){
+        $scope.itemlist = response;
+        $scope.item = "";
+      });
+    });
+  };
 
-	$scope.$on('handleBroadcast', function() {
-        	refresh();
+  $scope.sortDate = function(){
+    $http.get("/itemlist/date").success(function(response){
+      $scope.$applyAsync(function(){
+        $scope.itemlist = response;
+        $scope.item = "";
+      });
+    });
+  };
+
+  $scope.updateItem = function($element){
+    var current_id = ($element.itemID);
+    $http.put("/itemlist/" + current_id, $scope.item).success(function(response){
+      refresh();
+    });
+  };
+
+  $scope.status = '  ';
+  
+  $scope.clicked = function(ev,$element) {
+
+    var current_id = ($element.itemID);
+    console.log(current_id); 
+    $http.get("/itemlist/" + current_id).success(function(response){
+      var itemTags = [];
+      for(i=0;i<response.length;i++){
+        itemTags.push(response[i].tag);
+      }
+      $rootScope.itemTags = itemTags;
+
+      console.log("+++++++RESPONSE1++++++++");
+      console.log(response[0]);
+
+      if(response[0].claimed == 0){
+        response[0].claimed = false;
+      }
+      else{
+        response[0].claimed = true;
+      }
+
+      $rootScope.$applyAsync(function(){
+        $rootScope.item = response[0];
+      });
     });
 
-	$scope.isCollapsed = true;
-	
-}]).$inject = ['$scope', 'sharedServiceUpdateModal'];
+    $scope.parentSelected = $element;
+    console.log("PARENT SELECTED");
+    console.log($scope.parentSelected);
 
-angular.module('ItemApp').controller('ModalInstanceCtrl', function ($http,$rootScope,$scope, $uibModalInstance,$uibModal, items, sharedPropertiesTags) {
-	console.log("In TagsController from ItemListController");
-	console.log(items);
-	$rootScope.item = items;
-	$scope.selected = {
-	    item: $rootScope.item[0]
-	};
+    $mdDialog.show({
+      controller: ModalInstanceCtrl,
+      templateUrl: 'myModalContent.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      scope: $scope.$new()
+    })
+  };
+  $scope.showDetails = function ($element) {
+    if ($scope.active != $element.title) {
+      $scope.active = $element.title;
+    }
+    else {
+      $scope.active = null;
+    }
+  };
 
-	$scope.editItem = function ($element) {
-		var current_id = ($element.itemID); 
-		console.log($element);
-		$http.get("/itemlist/" + current_id).success(function(response){
+  $scope.$on('handleBroadcast', function() {
+    refresh();
+  });
 
-			$rootScope.$applyAsync(function(){
-				$rootScope.item = response[0];
-			});
-		});
+  $scope.isCollapsed = true;
+}).$inject = ['$scope', 'sharedServiceUpdateModal'];
 
-		var modalInstance = $uibModal.open({
-		    animation: $scope.animationsEnabled,
-		    templateUrl: 'myModalContentEdit.html',
-		    controller: 'ModalInstanceCtrl',
-		    size: 'md',
-		    resolve: {
-		        items: function () {
-		          	return $scope.item;
-		        }
-	      	}
-	    });
+function ModalInstanceCtrl($scope, $rootScope, $http, $mdDialog, sharedService, Upload, sharedServiceUploadModal,sharedPropertiesTags) {
+  
+  $rootScope.item = $scope.parentSelected;
+  $scope.item = $scope.parentSelected;
+  console.log($scope.parentSelected);
+  $scope.selected = {
+      item: $scope.parentSelected
+  };
 
-	    modalInstance.result.then(function ($rootScope) {
-	      $rootScope.selected = selectedItem;
-	    });
+  var itemEditTags = [];
 
-	    $scope.$on('handleBroadcast', function() {
-        	refresh();
-    	});
-	};
+  $scope.editItem = function (ev,$element) {
+    var current_id = ($element.itemID); 
+    console.log(current_id);
+    $http.get("/itemlist/" + current_id).success(function(response){
 
-  	$scope.updateItem = function($element){
-		var current_id = $rootScope.item.itemID;
-		console.log(current_id);
-		$http.put("/itemlist/" + current_id, $scope.item).success(function(response){
-			console.log("UPDATE");
-			$rootScope.$broadcast('handleBroadcast');
-			$uibModalInstance.dismiss('cancel');
-		});
-	};
+      for(i=0;i<response.length;i++){
+        itemEditTags.push({'name':response[i].tag});
+      }
+      console.log(itemEditTags);
+      $rootScope.selectedTags = itemEditTags;
 
-	$scope.removeItem = function($scope,$element){
-		var current_id = ($scope.itemID); 
-		console.log(current_id);
-		$http.delete("/itemlist/?" + current_id).success(function(response){
-			$rootScope.$broadcast('handleBroadcast');
-			$uibModalInstance.dismiss('cancel');
-		});
-	};
+      console.log("+++++++RESPONSE++++++++");
+      console.log(response[0].claimed);
 
- 	$scope.cancel = function () {
-    	$uibModalInstance.dismiss('cancel');
-  	};
+      if(response[0].claimed == 0){
+        response[0].claimed = false;
+      }
+      else{
+        response[0].claimed = true;
+      }
 
-
-  	var myList = [];
-
-    function getTagsFromDatabase() {
-      $http.get("/tags").success(function(response){
-        console.log("tags request received.");
-        for (object in response){
-          // myList.push({'name': response[object].tag});
-          myList.push({'name': response[object].tag});
-        }
-        sharedPropertiesTags.setProperty(myList);
-        $scope.allTags = loadTags(myList);
-
+      $rootScope.$applyAsync(function(){
+        $rootScope.item = response[0];
       });
+    });
+
+    $mdDialog.show({
+      controller: itemModalInstanceCtrl,
+      templateUrl: 'myModalContentEdit.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      scope: $rootScope.$new()
+    })
+  };
+
+  $scope.updateItem = function(ev,$element){
+    console.log("HI UPDATEITEM");
+    var current_id = $scope.parentSelected.itemID;
+    var fullTagsRaw = $rootScope.selectedTags;
+    $scope.parentSelected.newTags = fullTagsRaw;
+
+    $http.put("/itemlist/" + current_id, $scope.parentSelected).success(function(response){
+      $rootScope.$broadcast('handleBroadcast');
+      $uibModalInstance.dismiss('cancel');
+    });
+  };
+
+  $scope.removeItem = function($scope,$element){
+    var current_id = ($scope.itemID); 
+    $http.delete("/itemlist/?" + current_id).success(function(response){
+      $rootScope.$broadcast('handleBroadcast');
+      $uibModalInstance.dismiss('cancel');
+    });
+  };
+
+  var myList = [];
+
+  function getTagsFromDatabase() {
+    $http.get("/tags").success(function(response){
+      for (object in response){
+        // myList.push({'name': response[object].tag});
+        myList.push({'name': response[object].tag});
+      }
+      sharedPropertiesTags.setProperty(myList);
+      $scope.allTags = loadTags(myList);
+
+    });
+  };
+
+  getTagsFromDatabase();
+
+  $scope.readonly = false;
+  $scope.selectedItem = null;
+  $scope.searchText = null;
+  $scope.querySearch = querySearch;
+  $scope.numberBuffer = '';
+  $scope.autocompleteDemoRequireMatch = true;
+  $scope.transformChip = transformChip;
+  $scope.myTags = [];
+
+  /* Return the proper object when the append is called. */
+  function transformChip(chip) {
+    // If it is an object, it's already a known chip
+    if (angular.isObject(chip)) {
+      return chip;
+    }
+    // Otherwise, create a new one
+    return {tagname: chip};
+    // return {name: chip,type='color'};
+  }
+  /* Search for tags. */
+  function querySearch(query) {
+    var results = query ? $scope.allTags.filter(createFilterFor(query)) : [];
+    return results;
+  }
+  /* Create filter function for a query string */
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+    return function filterFn(tag) {
+      return (tag.lowername.indexOf(lowercaseQuery) === 0)
+    };
+  }
+  function loadTags(tagsList) {
+    var tags = tagsList;
+    return tags.map(function (tag) {
+      tag.lowername = tag.name.toLowerCase();
+      return tag;
+    });
+  }
+
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
+
+function itemModalInstanceCtrl($scope, $rootScope, $http, $mdDialog, sharedService, Upload, sharedServiceUploadModal,sharedPropertiesTags) {
+
+  var myList = [];
+
+  $scope.updateItem = function(ev,$element){
+    console.log($rootScope.item);
+    var current_id = $rootScope.item.itemID;
+    var fullTagsRaw = $rootScope.selectedTags;
+    $rootScope.item.newTags = fullTagsRaw;
+
+    $http.put("/itemlist/" + current_id, $rootScope.item).success(function(response){
+      $rootScope.$broadcast('handleBroadcast');
+      $mdDialog.cancel();
+    });
+  };
+
+  function getTagsFromDatabase() {
+    $http.get("/tags").success(function(response){
+      for (object in response){
+        myList.push({'name': response[object].tag});
+      }
+      sharedPropertiesTags.setProperty(myList);
+      $scope.allTags = loadTags(myList);
+
+    });
+  };
+
+  getTagsFromDatabase();
+
+  $scope.readonly = false;
+  $scope.selectedItem = null;
+  $scope.searchText = null;
+  $scope.querySearch = querySearch;
+  $scope.selectedTags = [];
+  $scope.numberBuffer = '';
+  $scope.autocompleteDemoRequireMatch = true;
+  $scope.transformChip = transformChip;
+  $scope.myTags = [];
+
+  /* Return the proper object when the append is called. */
+  function transformChip(chip) {
+    // If it is an object, it's already a known chip
+    if (angular.isObject(chip)) {
+
+      return chip;
+    }
+    // Otherwise, create a new one
+    return {tagname: chip};
+    // return {name: chip,type='color'};
+  }
+  /* Search for tags. */
+  function querySearch(query) {
+    var results = query ? $scope.allTags.filter(createFilterFor(query)) : [];
+    return results;
+  }
+  /* Create filter function for a query string */
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+    return function filterFn(tag) {
+      return (tag.lowername.indexOf(lowercaseQuery) === 0)
+    };
+  }
+  function loadTags(tagsList) {
+    var tags = tagsList;
+    return tags.map(function (tag) {
+      tag.lowername = tag.name.toLowerCase();
+      return tag;
+    });
+  }
+
+  $scope.uploadImage = function (file) {
+    if (file != null) {
+        $scope.upload(file)
+      }
+      else{
+        $scope.addItem();
+      }
+      $mdDialog.cancel();
     };
 
-    getTagsFromDatabase();
+  $scope.addItem = function(){
 
-    //console.log(sharedPropertiesTags.getProperty());
+    $scope.errorMsg = null;
+    var fullTagsRaw = $scope.selectedTags;
+    $scope.item.newTags = fullTagsRaw;
+    $http.post("/additem",$scope.item).success(function(response){
 
-    $scope.readonly = false;
-    $scope.selectedItem = null;
-    $scope.searchText = null;
-    $scope.querySearch = querySearch;
-    $scope.selectedTags = [];
-    $scope.numberBuffer = '';
-    $scope.autocompleteDemoRequireMatch = true;
-    $scope.transformChip = transformChip;
-    $scope.myTags = [];
+      sharedService.refreshMain();
+      $mdDialog.cancel();
+    });
+  };
 
-    /* Return the proper object when the append is called. */
-    function transformChip(chip) {
-      // If it is an object, it's already a known chip
-      if (angular.isObject(chip)) {
-        return chip;
-      }
-      // Otherwise, create a new one
-      return {tagname: chip};
-      // return {name: chip,type='color'};
-    }
-    /* Search for tags. */
-    function querySearch(query) {
-      var results = query ? $scope.allTags.filter(createFilterFor(query)) : [];
-      return results;
-    }
-    /* Create filter function for a query string */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(tag) {
-        return (tag.lowername.indexOf(lowercaseQuery) === 0)
-      };
-    }
-    function loadTags(tagsList) {
-      var tags = tagsList;
-      return tags.map(function (tag) {
-        tag.lowername = tag.name.toLowerCase();
-        return tag;
+  $scope.upload = function(image) {
+
+    $scope.errorMsg = null;
+    var transferredItem = sharedServiceUploadModal.getProperty();
+    var fullTagsRaw = $scope.selectedTags;
+    $scope.item.newTags = fullTagsRaw;
+    $http.post("/additem",$scope.item).success(function(response){
+      var fileExtension = '.' + image.name.split('.').pop();
+      Upload.rename(image, response.insertId.toString()+fileExtension);
+      image.upload = Upload.upload({
+        url: 'additem/uploadImage',
+        method: 'POST',
+        data: {
+        type: 'profile'
+        },
+        file: image
       });
-    }
+      sharedService.refreshMain();
 
+      $mdDialog.cancel();
+    });
+  };
 
+  $scope.getReqParams = function () {
+    return $scope.generateErrorOnServer ? '?errorCode=' + $scope.serverErrorCode +
+    '&errorMessage=' + $scope.serverErrorMsg : '';
+  };
 
-}).$inject = ['$scope', 'sharedServiceUpdateModal'];
-       
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+
+};
