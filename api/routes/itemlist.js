857762c29c72ac789ec1dae8caab5d2c5754afb8
sparrow <alevar@gmail.com>
2016-03-15 15:21:05 -0500

    Handling of the additional components items have now

--- api/routes/itemlist.js
+++ api/routes/itemlist.js
@@ -8,9 +8,65 @@ db.query('USE ' + dbconfig.database);
 
 module.exports = function(app, passport, isLoggedIn) {
     app.get("/itemlist", isLoggedIn, function(req,res){
-            console.log('i received the request');
 
             db.query('SELECT * from ItemLF', function(err, rows, fields) {
+                    
+                for (i = 0; i < rows.length; i++) { 
+                    if(rows[i].imagePrimColor == null){
+                        rows[i].currentImage = '../itemImages/'+ rows[i].itemColor + '.jpg';
+                    }
+                    else{
+                        rows[i].currentImage = '../itemImages/' + rows[i].itemID + '.jpg';
+                    }
+                }
+
+                //console.log(auth.user.username);
+                res.json(rows);
+            });
+
+    });
+
+    app.get("/itemlist/alpha", isLoggedIn, function(req,res){
+
+            db.query('SELECT * from ItemLF ORDER BY title ASC', function(err, rows, fields) {
+                    
+                for (i = 0; i < rows.length; i++) { 
+                    if(rows[i].imagePrimColor == null){
+                        rows[i].currentImage = '../itemImages/'+ rows[i].itemColor + '.jpg';
+                    }
+                    else{
+                        rows[i].currentImage = '../itemImages/' + rows[i].itemID + '.jpg';
+                    }
+                }
+
+                //console.log(auth.user.username);
+                res.json(rows);
+            });
+
+    });
+
+    app.get("/itemlist/location", isLoggedIn, function(req,res){
+
+            db.query('SELECT * from ItemLF ORDER BY locationID ASC', function(err, rows, fields) {
+                    
+                for (i = 0; i < rows.length; i++) { 
+                    if(rows[i].imagePrimColor == null){
+                        rows[i].currentImage = '../itemImages/'+ rows[i].itemColor + '.jpg';
+                    }
+                    else{
+                        rows[i].currentImage = '../itemImages/' + rows[i].itemID + '.jpg';
+                    }
+                }
+
+                //console.log(auth.user.username);
+                res.json(rows);
+            });
+
+    });
+
+    app.get("/itemlist/date", isLoggedIn, function(req,res){
+
+            db.query('SELECT * from ItemLF ORDER BY time_stamp ASC', function(err, rows, fields) {
                     
                 for (i = 0; i < rows.length; i++) { 
                     if(rows[i].imagePrimColor == null){
@@ -46,16 +102,31 @@ module.exports = function(app, passport, isLoggedIn) {
                         result[i].currentImage = '../itemImages/' + result[i].itemID + '.jpg';
                     }
                 }
-
+                console.log(result);
                 res.json(result);
             });
     });
 
     app.put('/itemlist/:id',function(req,res){
             var to_update = req.params["id"];
-            console.log(req.body.itemID);
+            var tags = req.body.newTags;
+            delete req.body["currentImage"];
+            delete req.body["tag"];
+            delete req.body["newTags"];
+            console.log(req.body);
+            console.log(tags);
             db.query("UPDATE ItemLF SET ? WHERE itemID = '" + to_update + "'", req.body, function(err,result){
-                    res.json(result);
+                    
+                for (var i = 0; i < tags.length; i++) {
+                    var newTag = tags[i].name;
+                    console.log(newTag);
+                    db.query("INSERT INTO ItemTags (itemID,tag) VALUES (" +
+                    to_update + ",'" + newTag + "');",function(err){
+
+                    });
+                }
+
+                res.json(result);
             });
     });
 
