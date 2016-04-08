a52bdc36af58b9be8c1ac9f0d7804c9e03ef29c1
sergeihanka <sergeiahanka@gmail.com>
2016-04-08 00:30:39 -0500

    committing minor changes to allow git pull

--- addTagsTable.sql
+++ addTagsTable.sql
@@ -1,8 +1,5 @@
 USE lost
 
-CREATE TABLE Tags (tag VARCHAR(15) NOT NULL PRIMARY KEY) 
-engine=MyISAM;
-
 INSERT INTO Tags (tag) VALUES ("Red");
 INSERT INTO Tags (tag) VALUES ("Black");
 INSERT INTO Tags (tag) VALUES ("Green");
@@ -14,29 +11,3 @@ INSERT INTO Tags (tag) VALUES ("Yellow");
 INSERT INTO Tags (tag) VALUES ("White");
 INSERT INTO Tags (tag) VALUES ("Brown");
 INSERT INTO Tags (tag) VALUES ("Gray");
-
-CREATE TABLE ItemTags (
-	itemID INT NOT NULL,
-	tag VARCHAR(15) NOT NULL,
-	FOREIGN KEY (itemID) REFERENCES ItemLF (itemID), 
-	FOREIGN KEY (tag) REFERENCES Tags (tag),
-	PRIMARY KEY (itemID, tag)
-	)
-engine=MyISAM;
-
-INSERT INTO ItemTags (itemID, tag) VALUES (1, "Black");
-INSERT INTO ItemTags (itemID, tag) VALUES (2, "Blue");
-INSERT INTO ItemTags (itemID, tag) VALUES (3, "Brown");
-INSERT INTO ItemTags (itemID, tag) VALUES (4, "Gray");
-INSERT INTO ItemTags (itemID, tag) VALUES (5, "Black");
-INSERT INTO ItemTags (itemID, tag) VALUES (6, "Blue");
-INSERT INTO ItemTags (itemID, tag) VALUES (7, "Brown");
-INSERT INTO ItemTags (itemID, tag) VALUES (8, "Gray");
-INSERT INTO ItemTags (itemID, tag) VALUES (9, "Black");
-INSERT INTO ItemTags (itemID, tag) VALUES (10, "Blue");
-INSERT INTO ItemTags (itemID, tag) VALUES (11, "Brown");
-INSERT INTO ItemTags (itemID, tag) VALUES (12, "Gray");
-INSERT INTO ItemTags (itemID, tag) VALUES (13, "Black");
-INSERT INTO ItemTags (itemID, tag) VALUES (14, "Blue");
-INSERT INTO ItemTags (itemID, tag) VALUES (15, "Brown");
-INSERT INTO ItemTags (itemID, tag) VALUES (16, "Gray");
--- testBuild.sql
+++ testBuild.sql
@@ -157,25 +157,15 @@ CREATE TABLE Tags (tag VARCHAR(15) NOT NULL PRIMARY KEY)
 engine=MyISAM;
 
 INSERT INTO Tags (tag) VALUES ("Red");
-
 INSERT INTO Tags (tag) VALUES ("Black");
-
 INSERT INTO Tags (tag) VALUES ("Green");
-
 INSERT INTO Tags (tag) VALUES ("Blue");
-
 INSERT INTO Tags (tag) VALUES ("Pink");
-
 INSERT INTO Tags (tag) VALUES ("Purple");
-
 INSERT INTO Tags (tag) VALUES ("Orange");
-
 INSERT INTO Tags (tag) VALUES ("Yellow");
-
 INSERT INTO Tags (tag) VALUES ("White");
-
 INSERT INTO Tags (tag) VALUES ("Brown");
-
 INSERT INTO Tags (tag) VALUES ("Gray");
 
 CREATE TABLE ItemTags (
