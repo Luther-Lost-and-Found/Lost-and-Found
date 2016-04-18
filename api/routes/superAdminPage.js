02f3605c5e327d690bf18c00f2842d54d3a6c31f
sparrow <alevar@gmail.com>
2016-04-18 00:59:50 -0500

    added support for super admin stuff

--- api/routes/superAdminPage.js
+++ api/routes/superAdminPage.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+	bcrypt = require('bcrypt-nodejs'),
 	path = require('path'),
 	mysql = require('mysql'),
 	dbconfig = require('../../config/database'),
-	db = mysql.createConnection(dbconfig.connection);
+	db = mysql.createConnection(dbconfig.connection),
+    salt = bcrypt.genSaltSync(10);
 	
 db.query('USE ' + dbconfig.database);
 
@@ -19,6 +21,108 @@ module.exports = function(app, passport, isLoggedIn, isSuper) {
 
     });
 
-	console.log("HELLO FROM SUPER ADMIN");
-}
+    function makeid(){
+	    var text = "";
+	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+	    for( var i=0; i < 5; i++ )
+	        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	    return text;
+	}
+
+    app.post("/resetPassword", isLoggedIn, isSuper, function(req,res){
+
+		console.log("CHANGING PASSWORD", req.body);
+		var user = req.body;
+
+		console.log(makeid());
+
+		db.query("SELECT * FROM AdminLF WHERE norsekeyID = ?",[user.norsekeyID], function(err, rows) {
+            if (err){
+                return done(err);
+            }
+            else {
+            	var newPassword_NONHASH = makeid();
+                var newPassword = bcrypt.hashSync(newPassword_NONHASH, salt);
+                console.log("HASHED PASSWORD", newPassword);
+
+                // var insertQuery = "INSERT INTO AdminLF ( username, password ) values (?,?)";
+
+                db.query("UPDATE AdminLF SET password = ? WHERE norsekeyID = '" + user.norsekeyID + "'",newPassword, function(err,result){
+	                console.log("PASSWORD CHANGED",result);
+	                res.json(newPassword_NONHASH);
+	            });   
+            }
+        });
+    });
+
+    app.post("/grantSuper", isLoggedIn, isSuper, function(req,res){
+
+		console.log("GRANTING SUPER", req.body);
+		var user = req.body;
+
+		db.query("SELECT * FROM AdminLF WHERE norsekeyID = ?",[user.norsekeyID], function(err, rows) {
+            if (err){
+                return done(err);
+            }
+            else {
+
+            	var currentPrivilege = parseInt(user.superPrivilege);
+            	var newPrivilege;
+
+            	if(currentPrivilege == 0){
+            		newPrivilege = 1;
+            	}
+            	else{
+            		newPrivilege = 0;
+            	}
+
+                db.query("UPDATE AdminLF SET superPrivilege = ? WHERE norsekeyID = '" + user.norsekeyID + "'",newPrivilege, function(err,result){
+	                console.log("PASSWORD CHANGED",result);
+	            });   
+            }
+        });
+    });
+
+    app.delete('/deleteUser', function(req, res) {
+    	var userID = Object.keys(req.query)[0];
+        console.log("DELETING SUPER",userID);
+
+		db.query("SELECT * FROM AdminLF WHERE norsekeyID = ?",[userID], function(err, rows) {
+            if (err){
+                return done(err);
+            }
+            else{
+
+                db.query("DELETE FROM AdminLF WHERE norsekeyID = '" + userID + "'", function(err,result){
+		            res.json(result);
+		        });   
+            }
+        });  
+    });
+
+//     app.post("/addUser", isLoggedIn, isSuper, function(req,res){
+
+// 		console.log("CHANGING PASSWORD", req.body);
+// 		var user = req.body;
+
+// 		console.log(makeid());
+
+//     	var newPassword_NONHASH = makeid();
+//         var newPassword = bcrypt.hashSync(newPassword_NONHASH, salt);
+//         console.log("HASHED PASSWORD", newPassword);
+
+//         // var insertQuery = "INSERT INTO AdminLF ( username, password ) values (?,?)";
+
+//         db.query("INSERT INTO ItemLF (title,locationID,accepted_by,itemColor,time_stamp) VALUES ('"+
+//                 req.body.title + "'," + currentLocation[0].locationID + ",'" + req.user.norsekeyID +
+//                 "','" + req.body.itemColor + "',CURDATE());", function(err,result){
+//             console.log("PASSWORD CHANGED",result);
+//             res.json(newPassword_NONHASH);
+//         });
+
+//         INSERT INTO LocationLF (locationID,phonenumber,email,building_name,room_name)
+// VALUES (1,"111-1111","awesome@location.edu","Miller","418");
+//     });
+}
--- public/controllers/superAdminController.js
+++ public/controllers/superAdminController.js
@@ -6,12 +6,46 @@ app.controller('superAdminController', ['$timeout', '$scope', '$http', '$window'
 
 	console.log("Hello World from the SUPER ADMIN Controller");
 
-	$http.get("/superAdminPage").success(function(response){
-		console.log("TRYING TO GET TO THE SUPER ADMIN PAGE", response);
-		$scope.AdminUsers = response;
-	});
+	var refresh = function(){
+		$http.get("/superAdminPage").success(function(response){
+			console.log("TRYING TO GET TO THE SUPER ADMIN PAGE", response);
+			$scope.AdminUsers = response;
+		});
+	}
+
+	refresh();
 
 	$scope.test = function (func, user) {
 		console.log(func+" : "+user);
+	};
+
+	$scope.resetPassword = function (user) {
+		console.log(user);
+		$http.post("/resetPassword",user).success(function(response){
+			console.log("RESETTING PASSWORD", response);
+			$scope.newPassword = response;
+		});
+	};
+
+	$scope.grantSuper = function (user) {
+		console.log(user);
+		$http.post("/grantSuper",user).success(function(response){
+			console.log("GRANTED SUPER", response);
+		});
+	};
+
+	$scope.deleteUser = function (user) {
+		console.log(user);
+		$http.delete("/deleteUser/?" + user.norsekeyID).success(function(response){
+			console.log("DELETING USER", response);
+			refresh();
+		});
+	};
+
+	$scope.addUser = function (newUser) {
+		console.log(user);
+		$http.post("/addUser",newUser).success(function(response){
+			console.log("CREATED NEW USER", response);
+		});
 	};
 }]);
