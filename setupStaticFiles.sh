9c16fb09e7b78d096028f0fb85aa131c56568fbf
Kirby Olson <riknos314@gmail.com>
2016-04-21 12:51:45 -0500

    clean setup script

--- setupStaticFiles.sh
+++ setupStaticFiles.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-# make a python virtual environment with
-# the requirements for the flask server
-#virtualenv -p $(which python2.7) venv
-#source venv/bin/activate
-#pip2 install -r requirements.txt
-
-# create the user for nginx
-#sudo useradd www-data
-
 # setup the nginx configuration file
 sudo mkdir -p /etc/nginx/sites-availiable /etc/nginx/sites-enabled
 sudo cp nginx/conf/nginx.conf /etc/nginx/nginx.conf
